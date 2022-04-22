b823b305406bd3a631d9477edd2ce1c3f93ccc08
Suguru Yajima <yajima@i-dig.jp>
2022-04-22 13:03:54 +0900

    [add]reset.cssの為のstyled-resetを追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
-import './index.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+  <>
+    <GlobalStyle />
+    <App />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
